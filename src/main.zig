8db7adff506e4136a89264725b70bc3d712d41f6
Tynahan <nusilite@gmail.com>
2026-03-30 21:49:06 +0900

    feat: encode perfect 3 bytes

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,127 @@
 const std = @import("std");
-const project_1_base64_encoder_decoder = @import("project_1_base64_encoder_decoder");
+
+pub fn countBase64EncodedChars(source: []const u8) !usize {
+    if (source.len < 3) {
+        return 4;
+    }
+
+    const ceil = try std.math.divCeil(usize, source.len, 3);
+    return ceil * 4;
+}
+
+pub fn countBase64DecodedBytes(source: []const u8) usize {
+    const ceil = std.math.ceil(source / 4);
+    var valid_length = ceil * 3;
+
+    while (true) {
+        const last_char = source[valid_length - 1];
+        if (last_char != '=') {
+            break;
+        }
+        valid_length -= 1;
+    }
+
+    return valid_length;
+}
+
+const Base64 = struct {
+    table: *const [64]u8,
+
+    pub fn init() Base64 {
+        const uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowers = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symbols = "0123456789+/";
+        return Base64{ .table = uppers ++ lowers ++ numbers_symbols };
+    }
+
+    pub fn charAt(self: Base64, index: u8) u8 {
+        return self.table[index];
+    }
+
+    // NOTE: need to take heap pointer
+    // NOTE: need to return heap pointer
+    pub fn encode3Byptes(self: Base64, source: []const u8) [4]u8 {
+        switch (source.len) {
+            1 => {
+                // NOTE: 굳이 마스킹을 해야 하나?
+                // TODO: 마스킹 하는 게 더 속 편하긴 할 것 같다. 이게 작동하는지 확인한 다음 적용해보자
+                const first = source[0] >> 2;
+                const second = (source[0] & 0b00000011) << 6;
+
+                const first_char = self.charAt(first);
+                const second_char = self.charAt(second);
+                const third_char = '=';
+                const fourth_char = '=';
+                return [4]u8{ first_char, second_char, third_char, fourth_char };
+            },
+            2 => {
+                const first = source[0] >> 2;
+                const second = ((source[0] & 0b00000011) << 6) + (source[1] >> 4);
+                const third = (source[1] & 0b00001111) << 4;
+
+                const first_char = self.charAt(first);
+                const second_char = self.charAt(second);
+                const third_char = self.charAt(third);
+                const fourth_char = '=';
+                return [4]u8{ first_char, second_char, third_char, fourth_char };
+            },
+            3 => {
+                const first = source[0] >> 2;
+                const second = ((source[0] & 0b00000011) << 4) + (source[1] >> 4);
+                const third = ((source[1] & 0b00001111) << 2) + (source[2] >> 6);
+                const fourth = (source[2] & 0b00111111);
+
+                const first_char = self.charAt(first);
+                const second_char = self.charAt(second);
+                const third_char = self.charAt(third);
+                const fourth_char = self.charAt(fourth);
+                return [4]u8{ first_char, second_char, third_char, fourth_char };
+            },
+            else => @panic("only length 1 ~ 3 is supported"),
+        }
+    }
+
+    // NOTE: main에서 바로 받을 거니까 compile time known
+    // TODO: user input 받아서 변환하는 걸 만들어보자
+    pub fn encode(self: Base64, allocator: std.mem.Allocator, source: []const u8) ![]u8 {
+        const group_length = try std.math.divCeil(usize, source.len, 3);
+        const encoded_char_count = try countBase64EncodedChars(source);
+        var encoded = try allocator.alloc(u8, encoded_char_count);
+        @memset(encoded, 0);
+        std.debug.print("group_length: {any}\n", .{group_length});
+        std.debug.print("encoded_char_count: {any}\n", .{encoded_char_count});
+        std.debug.print("encoded initialized: {any}\n", .{encoded});
+
+        var group_index: usize = 0;
+        while (group_index < group_length) {
+            std.debug.print("inside encode while loop, encoded: {any}\n", .{encoded});
+            // TODO: how can I fix it to length 3 array
+            const group_slice = source[group_index * 3 .. group_index * 3 + 3];
+            const group_encoded = self.encode3Byptes(group_slice);
+            std.debug.print("inside encode while loop, group_encoded_any: {any}\n", .{group_encoded});
+            std.debug.print("inside encode while loop, group_encoded_string: {s}\n", .{group_encoded});
+            std.debug.print("inside encode while loop, group_encoded length: {any}\n", .{group_encoded.len});
+
+            // NOTE: 3 바이트 당 네 글자가 된다
+            @memcpy(encoded[group_index * 4 .. group_index * 4 + group_encoded.len], group_encoded[0..]);
+            std.debug.print("end of while loop cycle, encoded: {any}\n", .{encoded});
+            group_index += 1;
+        }
+
+        std.debug.print("end of while loop, encoded: {any}\n", .{encoded});
+        return encoded;
+    }
+
+    // pub fn decode(self: Base64, allocator: std.mem.Allocator) []u8 {}
+};
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try project_1_base64_encoder_decoder.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    const base64 = Base64.init();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    const some_string = "abc";
+    const encoded = try base64.encode(allocator, some_string);
+    defer allocator.free(encoded);
+    std.debug.print("encoded_any: {any}\n", .{encoded});
+    std.debug.print("encoded_string: {s}\n", .{encoded});
 }
